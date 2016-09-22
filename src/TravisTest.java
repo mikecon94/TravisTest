51e2c85dee4cc667268b6fbefe52360be355a5e2
Mike Conroy <mike.conroy@capgemini.com>
2016-09-22 21:24:40 +0100

    Add travis file.

--- src/TravisTest.java
+++ src/TravisTest.java
@@ -1,5 +1,5 @@
 
-public class TravistTest {
+public class TravisTest {
 
 	public static void main(String[] args) {
 		System.out.println("Basic to learn how to get started with Travis and Maven");
