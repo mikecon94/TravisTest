7c4a124f11786605a9cc705643d3fb8d37bf7154
Mike Conroy <mike.conroy@capgemini.com>
2016-09-22 22:57:49 +0100

    Add JUnit Test class and update directory structure.

--- src/main/java/com/test/travis/Travis.java
+++ src/main/java/com/test/travis/Travis.java
@@ -1,5 +1,6 @@
+package com.test.travis;
 
-public class TravisTest {
+public class Travis {
 
 	public static void main(String[] args) {
 		System.out.println("Basic to learn how to get started with Travis and Maven");
---
+++ src/test/java/Test.java
@@ -0,0 +1,16 @@
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test1() {
+		assertEquals(1, 1);
+	}
+
+	@org.junit.Test
+	public void travisTest(){
+		assertTrue(1==1);
+	}
+	
+}
